400f04f60e343f7c50b88ca5206f56069d545d72
Mickey248 <mickeynaja246@gmail.com>
2017-04-24 20:40:12 +0700

    Add files via upload

---
+++ EditTwoAddrTH.php
@@ -0,0 +1,19 @@
+<?php
+
+	require'../Connect/dbConnect.php';//เชื่อมต่อฐานข้อมูล
+	//รับค่าจากFormมาใส่ในฐานข้อมูล
+	
+	$Header =$_POST['Header'];
+	$Detail =$_POST['Detail'];
+
+	$id_select=$_POST['id_select'];
+	$sql="update Addrth set Header='$Header',Detail='$Detail' where id=$id_select";
+	$result=mysqli_query($con,$sql);
+	
+	if($result){
+	echo"success";
+	echo "<script> window.location.assign('TableAddrTH.php'); </script>";
+	}else{
+		echo "เกิดข้อผิดพลาด".mysqli_error($con);
+	}
+?>
